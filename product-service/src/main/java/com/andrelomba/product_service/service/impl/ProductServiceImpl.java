69ba8f72cc668c932c6a428a6e39a6e0c4d36e0a
André Lomba <andrelombagr@gmail.com>
2025-05-22 14:40:40 -0300

    refactor: adjust logging

--- product-service/src/main/java/com/andrelomba/product_service/service/impl/ProductServiceImpl.java
+++ product-service/src/main/java/com/andrelomba/product_service/service/impl/ProductServiceImpl.java
@@ -41,11 +41,11 @@ public class ProductServiceImpl implements ProductService {
     ObjectId lastId = null;
     long totalExtractionDuration = 0;
     long totalKafkaDuration = 0;
-    LocalDateTime startDateTime = LocalDateTime.now();
+    String logFileTimestamp = LocalDateTime.now().format(fileDateFormatter);
     String filename = String.format("%s/extraction-and-kafka_%s.log",
-        startDateTime.format(fileDateFormatter), startDateTime.format(fileDateFormatter));
+        logFileTimestamp, logFileTimestamp);
 
-    writeLogHeader(filename, startDateTime);
+    writeLogHeader(filename, LocalDateTime.now().format(brDateFormatter));
     log.info("Iniciando extração...");
 
     while (true) {
@@ -54,24 +54,24 @@ public class ProductServiceImpl implements ProductService {
       try {
         batch = productRepository.findBatchAfterId(lastId, BATCH_SIZE);
       } catch (MongoTimeoutException e) {
-        log.error("O banco de dados está fora do ar! Sua Integração foi abortada.");
-        writeErrorLog(filename, "O banco de dados está fora do ar! Sua Integração foi abortada.", startDateTime, e);
-        writeLogFooter(filename, totalExtractionDuration, totalKafkaDuration, totalRecords, Status.ERROR);
+        writeErrorLog(filename, "O banco de dados está fora do ar! Sua Integração foi abortada.", logFileTimestamp,
+            lastId, e);
+        writeLogFooter(filename, totalExtractionDuration + (System.currentTimeMillis() - startBatchTime),
+            totalKafkaDuration, totalRecords, Status.ERROR);
         throw e;
       } catch (MongoException e) {
-        log.error("Ocorreram erros na extração do banco de dados.");
-        writeErrorLog(filename, "Ocorreram erros na extração do banco de dados.", startDateTime, e);
-        writeLogFooter(filename, totalExtractionDuration, totalKafkaDuration, totalRecords, Status.ERROR);
+        writeErrorLog(filename, "Ocorreram erros na extração do banco de dados.", logFileTimestamp, lastId, e);
+        writeLogFooter(filename, totalExtractionDuration + (System.currentTimeMillis() - startBatchTime),
+            totalKafkaDuration, totalRecords, Status.ERROR);
         throw e;
       }
-
       long endBatchTime = System.currentTimeMillis();
       long totalBatchDuration = endBatchTime - startBatchTime;
       totalExtractionDuration += totalBatchDuration;
+      totalRecords += batch.size();
 
       if (batch.isEmpty()) {
         writeLogFooter(filename, totalExtractionDuration, totalKafkaDuration, totalRecords, Status.SUCCESS);
-        log.info("Extração finalizada: {} registros em {} ms.", totalRecords, totalExtractionDuration);
         log.info("Envio ao Kafka finalizado: {} registros em {} ms.", totalRecords, totalKafkaDuration);
         break;
       }
@@ -80,31 +80,32 @@ public class ProductServiceImpl implements ProductService {
 
       long startBatchKafkaTime = System.currentTimeMillis();
       try {
-        kafkaProducerService.sendBatch(batch, batchId, startDateTime);
+        kafkaProducerService.sendBatch(batch, batchId, logFileTimestamp);
       } catch (KafkaUnmatchedCountException e) {
+        writeLogFooter(filename, totalExtractionDuration,
+            totalKafkaDuration + (System.currentTimeMillis() - startBatchKafkaTime), totalRecords, Status.ERROR);
         throw e;
       }
       long endBatchKafkaTime = System.currentTimeMillis();
       long totalBatchKafkaDuration = endBatchKafkaTime - startBatchKafkaTime;
       totalKafkaDuration += totalBatchKafkaDuration;
 
-      log.info("Lote de id {} extraído e enviado para o Kafka. Total: {}.", batchId, totalRecords);
-      writeLogRecordSuccess(filename, batchId, batch.size(), totalBatchDuration, totalBatchKafkaDuration);
-      totalRecords += batch.size();
       lastId = batch.get(batch.size() - 1).getId();
 
+      writeLogRecordSuccess(filename, batchId, batch.size(), totalBatchDuration, totalBatchKafkaDuration, totalRecords);
     }
   }
 
-  private void writeLogHeader(String filename, LocalDateTime startDateTime) {
+  private void writeLogHeader(String filename, String initTimestamp) {
     FileLogger.writeLog(filename,
         String.format(
             "==================================== Extração de registros em Lotes e Envio ao Kafka ====================================\n- Iniciado em: %s\n- Tamanho máximo de lote: %d\n=========================================================================================================================",
-            startDateTime.format(brDateFormatter), BATCH_SIZE));
+            initTimestamp, BATCH_SIZE));
   }
 
   private void writeLogRecordSuccess(String filename, String batchId, int batchSize, long extractionDuration,
-      long kafkaDuration) {
+      long kafkaDuration, int totalRecordsSoFar) {
+    log.info("Lote de id {} extraído e enviado para o Kafka. Total: {}.", batchId, totalRecordsSoFar);
     FileLogger.writeLog(filename,
         String.format("%s - Lote de id %s, tamanho %d, extraído em %d ms e enviado ao Kafka em %d ms",
             LocalTime.now().format(logTimeFormatter), batchId, batchSize, extractionDuration, kafkaDuration));
@@ -112,6 +113,7 @@ public class ProductServiceImpl implements ProductService {
 
   private void writeLogFooter(String filename, long extractionDuration, long kafkaDuration, int totalRecords,
       Status status) {
+    log.info("Extração finalizada: {} registros em {} ms.", totalRecords, extractionDuration);
     FileLogger.writeLog(filename,
         String.format(
             "=========================================================================================================================\n- Finalizado em: %s\n- Duração total da extração: %d ms\n- Duração total do envio ao Kafka: %d ms\n- Total de registros extraídos: %d\n- Status: %s\n=========================================================================================================================",
@@ -119,16 +121,23 @@ public class ProductServiceImpl implements ProductService {
             status.getName()));
   }
 
-  private void writeErrorLog(String mainLogFile, String customMessage, LocalDateTime processStartTime,
+  private void writeErrorLog(String mainLogFile, String customMessage, String logFileTimestamp, ObjectId lastSuccessId,
       Throwable ex) {
     String errorFile = String.format("%s/extraction-error_%s.log",
-        processStartTime.format(fileDateFormatter), processStartTime.format(fileDateFormatter));
-
-    String fullMessage = String.format("%s - Erro no banco de dados: %s\nExceção: %s\n",
+        logFileTimestamp, logFileTimestamp);
+    String fullMessage = String.format("%s - Erro no banco de dados: %s\nExceção: %s",
         LocalDateTime.now().format(logTimeFormatter),
         customMessage, ex.toString());
+
+    log.error(customMessage);
     FileLogger.writeLog(mainLogFile, fullMessage);
     FileLogger.writeLog(errorFile, fullMessage);
+
+    if (lastSuccessId != null) {
+      FileLogger.writeLog(errorFile,
+          String.format("- Último registro extraído com sucesso: %s", lastSuccessId.toString()));
+    }
+
     FileLogger.writeLog(errorFile, "Trace:");
     for (StackTraceElement line : ex.getStackTrace()) {
       FileLogger.writeLog(errorFile, line.toString());
