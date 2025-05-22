4760014dfab78d196b5a94a65e7f2794cd9dfb01
André Lomba <andrelombagr@gmail.com>
2025-05-22 14:44:57 -0300

    refactor: adjust consumer variables and method to persist invalid batch.

--- process-service/src/main/java/com/andrelomba/process_service/service/impl/KafkaConsumerService.java
+++ process-service/src/main/java/com/andrelomba/process_service/service/impl/KafkaConsumerService.java
@@ -47,6 +47,7 @@ public class KafkaConsumerService {
   public void consume(ConsumerRecord<String, String> record, Acknowledgment ack) {
     String batchId = getHeader(record, "batchId");
     String type = getHeader(record, "type");
+    String logFileTimestamp = getHeader(record, "logFileTimestamp");
 
     if (batchId == null || type == null) {
       log.warn("Mensagem ignorada por falta de headers obrigatórios: {}", record);
@@ -56,7 +57,7 @@ public class KafkaConsumerService {
     switch (type) {
       case "HEADER" -> handleHeader(batchId, record.value());
       case "DATA" -> handleData(batchId, record.value(), record);
-      case "FOOTER" -> handleFooter(batchId, ack);
+      case "FOOTER" -> handleFooter(batchId, record.value(), logFileTimestamp, ack);
       default -> log.warn("Tipo de mensagem desconhecido: {}", type);
     }
   }
@@ -82,7 +83,7 @@ public class KafkaConsumerService {
   }
 
   // DATA: Processa o registro e armazena em memória
-  private void handleData(String batchId, String json, ConsumerRecord<String, String> record) {
+  private void handleData(String batchId, String value, ConsumerRecord<String, String> record) {
     List<ProcessProduct> buffer = batchBuffer.get(batchId);
     if (buffer == null) {
       log.warn("Recebido DATA para batch {} sem HEADER válido.", batchId);
@@ -90,9 +91,9 @@ public class KafkaConsumerService {
     }
 
     try {
-      String id = JsonFormatter.extractValueFromJson(json, "id");
-      String name = JsonFormatter.extractValueFromJson(json, "name");
-      LocalDateTime createdAt = LocalDateTime.parse(JsonFormatter.extractValueFromJson(json, "createdAt"));
+      String id = JsonFormatter.extractValueFromJson(value, "id");
+      String name = JsonFormatter.extractValueFromJson(value, "name");
+      LocalDateTime createdAt = LocalDateTime.parse(JsonFormatter.extractValueFromJson(value, "createdAt"));
 
       buffer.add(new ProcessProduct(batchId, id, name, createdAt, LocalDateTime.now()));
       successCount.compute(batchId, (k, v) -> v + 1);
@@ -104,35 +105,34 @@ public class KafkaConsumerService {
   }
 
   // FOOTER: Finaliza e grava log apenas uma vez por batch
-  private void handleFooter(String batchId, Acknowledgment ack) {
+  private void handleFooter(String batchId, String value, String logFileTimestamp, Acknowledgment ack) {
     // Se não houver HEADER ou já foi processado antes
     if (!expectedCount.containsKey(batchId)) {
       log.warn("FOOTER recebido sem HEADER ou batch já finalizado: {}", batchId);
       return;
     }
 
-    int expected = expectedCount.get(batchId);
+    int expectedCountFooter = Integer.parseInt(JsonFormatter.extractValueFromJson(value, "recordsCount"));
+    int expectedCountHeader = expectedCount.get(batchId);
     int success = successCount.getOrDefault(batchId, 0);
     int error = errorCount.getOrDefault(batchId, 0);
     long duration = System.currentTimeMillis() - startTime.getOrDefault(batchId, System.currentTimeMillis());
-    String logFile = "kafka/kafka-consumer.log";
+    String logFile = String.format("%s/kafka-consumer_%s.log",
+        logFileTimestamp,
+        logFileTimestamp);
 
     // Verifica se todos os registros esperados chegaram
-    if (success + error == expected) {
+    if (success + error == expectedCountHeader && expectedCountHeader == expectedCountFooter) {
       FileLogger.writeLog(logFile, String.format(
           "%s - Lote de id %s, sucesso: %d, erros: %d, processado em %d ms",
           LocalTime.now().format(logTimeFormatter), batchId, success, error, duration));
-
       if (success > 0) {
-        processProductService.insertBatch(batchBuffer.get(batchId), batchId);
+        processProductService.insertBatch(batchBuffer.get(batchId), batchId, logFileTimestamp);
       }
-
-      ack.acknowledge(); // Confirma o processamento do lote no Kafka
     } else {
-      log.error("Contagem inconsistente para batch {}: esperados {}, recebidos {}.",
-          batchId, expected, success + error);
-      persistInvalidBatch(batchId, batchBuffer.get(batchId));
+      persistInvalidBatch(batchId, expectedCountHeader, success + error, logFileTimestamp, batchBuffer.get(batchId));
     }
+    ack.acknowledge();
 
     // Remove todos os dados do batch
     cleanupBatch(batchId);
@@ -156,17 +156,25 @@ public class KafkaConsumerService {
     }
   }
 
-  private void persistInvalidBatch(String batchId, List<ProcessProduct> buffer) {
-    Path path = Path.of("kafka/errors_batch_" + batchId + ".log");
-    try {
-      List<String> lines = buffer.stream()
-          .map(p -> String.format("{\"batchId\":\"%s\",\"productId\":\"%s\",\"processedAt\":\"%s\"}",
-              p.getBatchId(), p.getProductId(), p.getProcessedAt()))
-          .toList();
-      Files.write(path, lines, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
-    } catch (IOException e) {
-      log.error("Falha ao gravar batch {} com erro: {}", batchId, e.getMessage(), e);
+  private void persistInvalidBatch(String batchId, int expectedCount, int receivedCount, String logFileTimestamp,
+      List<ProcessProduct> buffer) {
+    String logFile = String.format("%s/kafka-consumer-batch-errors_%s.log",
+        logFileTimestamp,
+        logFileTimestamp);
+
+    log.error("Contagem inconsistente para batch {}: esperados {}, recebidos {}.",
+        batchId, expectedCount, receivedCount);
+
+    FileLogger.writeLog(logFile,
+        "====================================================================================================");
+    FileLogger.writeLog(logFile,
+        String.format("%s - Contagem inconsistente para batch %s: esperados %d, recebidos %d.",
+            LocalDateTime.now().format(logTimeFormatter), batchId, expectedCount, receivedCount));
+    for (ProcessProduct p : buffer) {
+      FileLogger.writeLog(logFile, p.toJson());
     }
+    FileLogger.writeLog(logFile,
+        "====================================================================================================");
   }
 
   private String getHeader(ConsumerRecord<String, String> record, String key) {
